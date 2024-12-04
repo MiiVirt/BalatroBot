539159a4aed8ff963d8ad10366b0525aa5de277a
Miikka Virtanen <32563239+MiiVirt@users.noreply.github.com>
2024-12-04 14:38:24 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 
 class Card:
-    def __init__(self, name, value, type):
+    def __init__(self, name, value, type, short):
         self.name = name
         self.value = value
         self.type = type
+        self.short = short
+    def __str__(self):
+        return f"{self.name}{self.value}{self.type}"
 
-card1 = Card("ace", 11, 1)
-card2 = Card("two", 2, 2)
-card3 = Card("three", 3, 3)
-card4 = Card("four", 4, 4)
-card5 = Card("five", 5, 5)
-card6 = Card("six", 6, 6)
-card7 = Card("seven", 7, 7)
-card8 = Card("eight", 8, 8)
-card9 = Card("nine", 9, 9)
-card10 = Card("ten", 10, 10)
-card11 = Card("jack", 10, 11)
-card12 = Card("queen", 10, 12)
-card13 = Card("king", 10, 13)
+
+c1 = Card("ace", 11, 1, "a")
+c2 = Card("two", 2, 2, "2")
+c3 = Card("three", 3, 3, "3")
+c4 = Card("four", 4, 4, "4")
+c5 = Card("five", 5, 5, "5")
+c6 = Card("six", 6, 6, "6")
+c7 = Card("seven", 7, 7, "7")
+c8 = Card("eight", 8, 8, "8")
+c9 = Card("nine", 9, 9, "9")
+c10 = Card("ten", 10, 10, "10")
+c11 = Card("jack", 10, 11, "j")
+c12 = Card("queen", 10, 12, "q")
+c13 = Card("king", 10, 13, "k")
+
+cards = [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13]
 
 def hand_values():
     # base chip amount, base mult, + chips, + mult
@@ -33,36 +39,43 @@ def hand_values():
     flush_house = [140, 14, 0, 0]
     flush_five = [160, 16, 0, 0]
 
-def hand():
-    print("2-10, jack = j, queen = q, king = k, ace = a")
-    print("Hearts = h, diamonds = d, clubs = c, spades = s")
-    print("example answer: qh = queen of hearts, 5d = five of diamonds ")
-    print("x = no card")
-    card1 = input("Card 1: ")
-    rank1, suit1 = card_type(card1)
-    card2 = input("Card 2: ")
-    rank2, suit2 = card_type(card2)
-    card3 = input("Card 3: ")
-    rank3, suit3 = card_type(card3)
-    card4 = input("Card 4: ")
-    rank4, suit4 = card_type(card4)
-    card5 = input("Card 5: ")
-    rank5, suit5 = card_type(card5)
-
-    ranks = [rank1, rank2, rank3, rank4, rank5]
-    suits = [suit1, suit2, suit3, suit4, suit5]
-
-    if suit1 == suit2 and suit2 == suit3 and suit3 == suit4 and suit4 == suit5:
+def hand(ranks, suits):
+    if suits[0] == suits[1] and suits[1] == suits[2] and suits[2] == suits[3] and suits[3] == suits[4]:
         print("flush!")
 
-    for i in suits:
-        
+
 def card_type(card):
     rank = card[0]
     suit = card[1]
     # check if correct values
     return rank, suit
 
+def main():
+    print("2-10, jack = j, queen = q, king = k, ace = a")
+    print("Hearts = h, diamonds = d, clubs = c, spades = s")
+    print("example answer: qh = queen of hearts, 5d = five of diamonds ")
+    print("xx = no card")
+    card1 = input("Card 1: ").lower()
+    rank1, suit1 = card_type(card1)
+    card2 = input("Card 2: ").lower()
+    rank2, suit2 = card_type(card2)
+    card3 = input("Card 3: ").lower()
+    rank3, suit3 = card_type(card3)
+    card4 = input("Card 4: ").lower()
+    rank4, suit4 = card_type(card4)
+    card5 = input("Card 5: ").lower()
+    rank5, suit5 = card_type(card5)
+
+    ranks = [rank1, rank2, rank3, rank4, rank5]
+    suits = [suit1, suit2, suit3, suit4, suit5]
+    hand(ranks, suits)
+
+    for i in ranks:
+        for c in cards:
+            if c.short == i:
+                print(c.name)
+
+
 
 if __name__ == '__main__':
-    hand()
+    main()
